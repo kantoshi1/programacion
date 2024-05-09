1009294a682cb2e88571b58320ab468f32b3a4e9
Kantoshi <migueleblair@hotmail.com>
2024-05-09 14:56:27 -0500

    Perimetro añadido

--- circulo.py
+++ circulo.py
@@ -2,4 +2,6 @@ pi = 3.14
 radio = float(input("Ingrese el radio en cm: "))
 Area = pi * (radio * radio)
 Resultado = Area
-print ("El area del circulo es", Resultado, "cm.")
+print ("El area del circulo es", Resultado, "cm^2.")
+Perimetro = pi * 2 * (radio)
+print ("El perimetro del circulo es", Perimetro, "cm.") 
